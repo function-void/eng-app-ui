c4a915053cd37d4a600ebb12a7de200af06a71e8
Amirbek Akhmetov <lieberkz99@gmail.com>
2023-05-14 13:27:16 +0600

    Feature complete

--- src/app/pages/main/grammar/lesson/lesson.component.ts
+++ src/app/pages/main/grammar/lesson/lesson.component.ts
@@ -7,6 +7,8 @@ import { LessonService } from "../../../../../api/services/LessonService";
 import { DictionaryService } from 'src/api/services/DictionaryService';
 import { Lesson } from 'src/api/model/Lesson';
 import { EnglishLevel } from 'src/api/model/EnglishLevel';
+import { Test } from 'src/api/model/Test';
+import { TestService } from 'src/api/services/TestService';
 
 @Component({
   selector: 'app-lesson',
@@ -18,22 +20,25 @@ export class LessonComponent implements OnInit, OnDestroy {
 
   lessons: Lesson[] = [];
   englishLevels: EnglishLevel[] = [];
+  tests: Test[] = [];
 
   constructor(
     private dataService: DataService,
     private lessonService: LessonService,
     private dictionaryService: DictionaryService,
+    private testService: TestService,
     private router: Router,
     private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.dataService.subscriber$.pipe(takeUntil(this.unsubscribe)).subscribe(data => {
+    this.dataService.subscriber$.pipe(takeUntil(this.unsubscribe)).subscribe(x => {
       this.getLessons();
     });
 
     this.getLessons();
     this.getAllLevels();
+    this.getAllTests();
   }
 
   ngOnDestroy(): void {
@@ -53,8 +58,16 @@ export class LessonComponent implements OnInit, OnDestroy {
     })
   }
 
+  getAllTests() {
+    this.tests = this.testService.getAllTests();
+  }
+
   navigateUnit(lesson: Lesson) {
     this.dataService.LessonId = lesson.id;
     this.router.navigate([this.router.url, 'unit']);
+  }
+
+  navigateGoogleDocs(url: string) {
+    location.href = url
   }
 }
